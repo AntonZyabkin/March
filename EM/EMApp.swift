e44bf8102d29f786e3dc7fb5a7f2d3cf075efd7a
Anatan <zyabkinantonv@gmail.com>
2023-03-13 09:27:22 +0300

    Create projecr, pod install

---
+++ EM/EMApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMApp.swift
+//  EM
+//
+//  Created by Anton Zyabkin on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct EMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
